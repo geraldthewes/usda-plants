ec949dd3cad5ac471deac6ec1622083f55e12705
Gerald Hewes <gerald@geraldthewes.org>
2024-12-08 11:12:49 -0500

    Download code now works, but is a bit verbose, will need to clean up

--- scrape.py
+++ scrape.py
@@ -4,11 +4,12 @@ import os
 import json
 from tqdm import tqdm
 import argparse
+from requests_toolbelt.utils import dump
 
 
 data_types = [
     { "name": "distribution",
-      "path": "getDownloadDistributionDocumentation",
+      "path": "PlantProfile/getDownloadDistributionDocumentation",
       "data": '{{"Field":"Symbol","SortBy":"sortSciName","Offset":null,"MasterId":"{}"}}',
       "accept": "text/csv"
      },
@@ -44,7 +45,7 @@ data_types = [
     }
 ]
 
-
+image_path='https://plants.sc.egov.usda.gov/'
 
 def get_unique_symbols(csv_file):
     symbols = set()
@@ -69,17 +70,34 @@ def get_id_for_symbol(symbol):
         return None
 
 
-def get_json_data(id, property):
+def get_json_data(id, property, debug=False):
     print(property)
     action = property["path"]
     base = f'https://plantsservices.sc.egov.usda.gov/api/{action}' 
     url =  base.format(id)
     headers = {'Accept': f'{property["accept"]}'}
+    if debug:
+        print(base)
+        print(headers)
     if property["data"]:
+        headers["Origin"] = "https://plants.usda.gov"
+        headers["Referer"] = "https://plants.usda.gov/"
+        headers['Sec-Fetch-Dest'] = 'empty' 
+        headers['Sec-Fetch-Mode'] =  'cors' 
+        headers['Sec-Fetch-Site'] = 'same-site'
+        headers['DNT'] = "1"
+        headers['Accept-Language'] = 'en-US,en;q=0.9'
+
         post = property["data"].format(id)
         post_data = json.loads(post)
+        if debug:
+            print(post)
+            print(headers)
         headers['Content-Type'] = 'application/json'
-        response = requests.put(url, data=post_data, headers=headers)
+        response = requests.post(url, json=post_data, headers=headers)
+        if debug:
+            data = dump.dump_all(response)
+            print(data.decode('utf-8'))
     else:
         response = requests.get(url, headers=headers)        
     if response.status_code == 200:
@@ -89,26 +107,32 @@ def get_json_data(id, property):
             return response.text
     else:
         name = property["name"]
+        print(response.text)
         print(f"Failed to get {name} for ID {id}. Status code: {response.status_code}")
         return None
 
+def download_single_image(save_dir, img_url):
+    image_url = f'{image_path}/{img_url}'
+    img_response = requests.get(image_url)
+    if img_response.status_code == 200:
+        img_name = os.path.join(save_dir, os.path.basename(img_url))
+        with open(img_name, 'wb') as f:
+            f.write(img_response.content)
+    else:
+        print(f"Failed to download image from {img_url}. Status code: {img_response.status_code}")
+    
+    
 def download_images(symbol, images_json, save_dir):
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    
     for img_data in tqdm(images_json, desc=f"Downloading images for {symbol}", unit="image"):
-        img_url = img_data.get('url')
-        if img_url:
-            img_response = requests.get(img_url)
-            if img_response.status_code == 200:
-                img_name = os.path.join(save_dir, img_data.get('filename', f'image_{hash(img_url)}.jpg'))
-                with open(img_name, 'wb') as f:
-                    f.write(img_response.content)
-            else:
-                print(f"Failed to download image from {img_url}. Status code: {img_response.status_code}")
-    
+        for (image) in ["StandardSizeImageLibraryPath",
+                         "ThumbnailSizeImageLibraryPath",
+                         "LargeSizeImageLibraryPath",
+                         "OriginalSizeImageLibraryPath"]:
+            img_url = img_data.get(image)
+            if img_url:
+                download_single_image(save_dir, img_url)
 
-def process_symbol(output_dir, symbol):
+def process_symbol(output_dir, symbol, debug=False):
     id = get_id_for_symbol(symbol)
     if not id:
         print(f'No id for sumbol {symbol}')
@@ -130,7 +154,7 @@ def process_symbol(output_dir, symbol):
             json.dump(profile_json, f, indent=4)
 
     for data_type in data_types:
-        data = get_json_data(id, data_type)
+        data = get_json_data(id, data_type, debug)
         if data:
             document = data_type["name"]
             with open(f'{symbol_dir}/{document}.json', 'w') as f:
@@ -171,7 +195,7 @@ if __name__ == '__main__':
     
     if args.symbol:
         print(f"Processing symbol: {args.symbol}")        
-        process_symbol(args.output_dir, args.symbol)
+        process_symbol(args.output_dir, args.symbol, True)
     else:
         print(f"Processing list from CSV: {args.csv_file}")        
         process_list(args.output_dir, args.csv_file)
